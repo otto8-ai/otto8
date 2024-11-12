3e83964f39a9ee3803e167003ee5291cfddb0bab
Ryan Hopper-Lowe <ryan@acorn.io>
2024-11-12 16:31:58 -0600

    feat: implement workflow chat interface

--- ui/admin/app/components/chat/Chat.tsx
+++ ui/admin/app/components/chat/Chat.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { cn } from "~/lib/utils";
+
 import { useChat } from "~/components/chat/ChatContext";
 import { Chatbar } from "~/components/chat/Chatbar";
 import { MessagePane } from "~/components/chat/MessagePane";
@@ -9,16 +11,23 @@ type ChatProps = React.HTMLAttributes<HTMLDivElement> & {
     showStartButton?: boolean;
 };
 
-export function Chat({ className, showStartButton = false }: ChatProps) {
-    const { messages, threadId, mode, invoke, readOnly } = useChat();
+export function Chat({ className }: ChatProps) {
+    const {
+        messages,
+        threadId,
+        mode,
+        invoke,
+        readOnly,
+        isInvoking,
+        isRunning,
+    } = useChat();
     const [runTriggered, setRunTriggered] = useState(false);
 
     const showMessagePane =
         mode === "agent" ||
-        (mode === "workflow" && (threadId || runTriggered || !showStartButton));
+        (mode === "workflow" && (threadId || runTriggered || !readOnly));
 
-    const showStartButtonPane =
-        mode === "workflow" && showStartButton && !(threadId || runTriggered);
+    const showStartButtonPane = mode === "workflow" && !readOnly;
 
     return (
         <div className={`flex flex-col h-full ${className}`}>
@@ -34,13 +43,22 @@ export function Chat({ className, showStartButton = false }: ChatProps) {
             {mode === "agent" && !readOnly && <Chatbar className="px-20" />}
 
             {showStartButtonPane && (
-                <div className="flex justify-center items-center h-full px-20">
+                <div
+                    className={cn("px-20 mb-4", {
+                        "flex justify-center items-center h-full": !threadId,
+                    })}
+                >
                     <Button
                         variant="secondary"
                         onClick={() => {
                             setRunTriggered(true);
                             invoke();
                         }}
+                        className={cn({
+                            "w-full": threadId,
+                        })}
+                        loading={isInvoking || isRunning}
+                        disabled={isInvoking || isRunning}
                     >
                         Run
                     </Button>
